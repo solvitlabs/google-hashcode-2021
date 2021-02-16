3ef0cd669b35f525695eae9b7fdfb829bfb2e1a6
d3vkk <donaldkk22@yahoo.com>
2021-02-16 17:47:08 +0300

    refactor: Create combinations of ingredients

--- create-combinations/T2.js
+++ create-combinations/T2.js
@@ -1,16 +1,18 @@
 // For a team of two
-// const letters = ["a", "b", "c", "d", "e"];
-// const letters = ["a", "b", "c"];
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+var convertToObject = require('./convertToObject');
+// const ingredientsBatch = ["a", "b", "c", "d", "e"];
+// const ingredientsBatch = ["a", "b", "c"];
+// const ingredientsBatch = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+const ingredientsBatch = convertToObject();
 
 var combinationsArr = [];
-//first loop to select letters
-while (letters.length != 0) {
-    var selectedLetter = letters[0];
-    letters.splice(0, 1);
-    //second loop to combine letters
-    for (let k = 0; k < letters.length; k++) {
-        combinationsArr.push(selectedLetter + letters[k])
+//first loop to select ingredientsBatch
+while (ingredientsBatch.length != 0) {
+    var selectedId = ingredientsBatch[0].id;
+    ingredientsBatch.splice(0, 1);
+    //second loop to combine ingredientsBatch
+    for (let k = 0; k < ingredientsBatch.length; k++) {
+        combinationsArr.push(selectedId + '-' + ingredientsBatch[k].id)
     }
 }
 console.log(combinationsArr);
